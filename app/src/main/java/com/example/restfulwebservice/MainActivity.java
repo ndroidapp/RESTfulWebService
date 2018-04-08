da9d211182e60b0c47a7c6bc64a6e2457caa7642
ndroidapp <ndroidapp.in@gmail.com>
2018-04-08 20:23:02 +0530

    AsyncTaskLoader Example

--- app/src/main/java/com/example/restfulwebservice/MainActivity.java
+++ app/src/main/java/com/example/restfulwebservice/MainActivity.java
@@ -1,51 +1,75 @@
 package com.example.restfulwebservice;
 
-import android.os.AsyncTask;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<String> {
 
-    private TextView output;
+    TextView output;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.output = findViewById(R.id.output);
 
+        output = (TextView) findViewById(R.id.output);
     }
 
     public void runClickHandler(View view) {
-//        output.append("Button Clicked\n");
-        MyAsyncTask task=new MyAsyncTask();
-        task.execute("String 1","String 2","String 3");
+        getSupportLoaderManager().restartLoader(0, null, this).forceLoad();
     }
 
     public void clearClickHandler(View view) {
         output.setText("");
     }
-    private class MyAsyncTask extends AsyncTask<String,String,Void> {
 
-        @Override
-        protected Void doInBackground(String... strings) {
-            for (String string : strings) {
-                publishProgress(string);
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-            return null;
+    @Override
+    public Loader<String> onCreateLoader(int id, Bundle args) {
+        output.append("creating loader\n");
+        return new MyTaskLoader(this);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<String> loader, String data) {
+        output.append("loader finished, returned: " + data + "\n");
+    }
+
+    @Override
+    public void onLoaderReset(Loader<String> loader) {
+
+    }
+
+    private static class MyTaskLoader extends AsyncTaskLoader<String> {
+
+        public MyTaskLoader(Context context) {
+            super(context);
         }
 
         @Override
-        protected void onProgressUpdate(String... values) {
-            super.onProgressUpdate(values);
-            output.append(values[0]+"\n");
+        public String loadInBackground() {
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            return "from the loader";
+        }
+
+        @Override
+        public void deliverResult(String data) {
+            data += ", delivered";
+            super.deliverResult(data);
         }
     }
+
 }
